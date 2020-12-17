0f53f6a99dee313ad26281af150fc66ed49ded98
Елисей Ехлаков <elisey.ekhlakov@krit.pro>
2020-12-17 09:48:06 +0400

    Lesson 28

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 'use strict';
 
 const box = document.getElementById('box');
-
 console.log(box);
+
+const btns = document.getElementsByTagName('button');
+console.log(btns[0]);
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+hearts.forEach(item=>{console.log(item);});
+
+const oneheart = document.querySelector('.heart');
+console.log(oneheart);
