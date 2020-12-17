51e4d539fcb8c54923b0c35c0ea2bc4103afb891
Елисей Ехлаков <elisey.ekhlakov@krit.pro>
2020-12-17 09:57:21 +0400

    emer

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
 'use strict';
 
 const box = document.getElementById('box');
-console.log(box);
-
 const btns = document.getElementsByTagName('button');
-console.log(btns[0]);
-
 const circles = document.getElementsByClassName('circle');
-console.log(circles);
-
 const hearts = document.querySelectorAll('.heart');
-hearts.forEach(item=>{console.log(item);});
+const oneHeart = document.querySelector('.heart');
 
-const oneheart = document.querySelector('.heart');
-console.log(oneheart);
+console.dir(box);
+
